4bd280875576abd56fd587fb734f4fab57b29b84
recca <flamerecca711@gmail.com>
2025-12-14 05:59:45 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+
+wp plugin install all-in-one-seo-pack --activate --allow-root
+wp plugin install all-in-one-wp-migration --activate --allow-root
+wp plugin install all-in-one-wp-security-and-firewall --activate --allow-root
+wp plugin install amp --activate --allow-root
+wp plugin install elementor --activate --allow-root
+wp plugin install google-analytics-for-wordpress --activate --allow-root
+wp plugin install smart-slider-3 --activate --allow-root
+wp plugin install simple-tags --activate --allow-root
+wp plugin install url-shortify --activate --allow-root
+wp plugin install w3-total-cache --activate --allow-root
+wp plugin install wp-mail-smtp --activate --allow-root
+wp plugin install wpforms-lite --activate --allow-root
+
+wp theme install astra --activate --allow-root
