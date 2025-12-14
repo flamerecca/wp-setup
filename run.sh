f4a9c4a3b217aee522a908ae35ba881a359f512a
recca <flamerecca711@gmail.com>
2025-12-14 22:01:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,5 +12,6 @@ wp plugin install url-shortify --activate --allow-root
 wp plugin install w3-total-cache --activate --allow-root
 wp plugin install wp-mail-smtp --activate --allow-root
 wp plugin install wpforms-lite --activate --allow-root
+wp plugin install health-endpoint --activate --allow-root
 
 wp theme install astra --activate --allow-root
